5eaa33743151c35a8fdb4cc0d04d0df4646ded8b
marcoskkk <marcos.kashima@kellogg.northwestern.edu>
2022-02-04 14:49:00 +0000

    fixed database

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,9 @@ ActiveRecord::Schema.define(version: 2022_02_01_172203) do
   end
 
   create_table "salespeople", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
